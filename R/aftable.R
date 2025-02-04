43908309efce50446a09a7e7e980594ee4d8ff36
Olivia Box Power <Olivia.BoxPower@dhsc.gov.uk>
2025-02-04 16:41:16 +0000

    Prevent check note about long line in function example

--- R/aftable.R
+++ R/aftable.R
@@ -111,14 +111,16 @@
 #'   check.names = FALSE
 #' )
 #'
+#'
 #' table_1_df <- data.frame(
 #'   Category = LETTERS[1:10],
 #'   "Numeric [note 1]" = 1:10,
 #'   "Numeric suppressed" = c(1:4, "[c]", 6:9, "[x]"),
 #'   "Numeric thousands" = abs(round(rnorm(10), 4) * 1e5),
 #'   "Numeric decimal" = abs(round(rnorm(10), 5)),
-#'   "This column has a very long name that means that the column width needs to be widened" = 1:10,
-#'   Notes = c("[note 1]", rep(NA_character_, 4), "[note 2]", rep(NA_character_, 4)),
+#'   "Long name that means that the column width needs to be widened" = 1:10,
+#'   Notes = c("[note 1]", rep(NA_character_, 4), "[note 2]",
+#'             rep(NA_character_, 4)),
 #'   check.names = FALSE
 #' )
 #'
@@ -126,39 +128,40 @@
 #'
 #' # Create 'aftables' object
 #'
-#' x <-
-#'   aftables::create_aftable(
-#'     tab_titles = c("Cover", "Contents", "Notes", "Table_1", "Table_2"),
-#'     sheet_types = c("cover", "contents", "notes", "tables", "tables"),
-#'     sheet_titles = c(
-#'       "The 'aftables' Demo Workbook",
-#'       "Table of contents",
-#'       "Notes",
-#'       "Table 1: First Example Sheet",
-#'       "Table 2: Second Example Sheet"
+#' x <- aftables::create_aftable(
+#'   tab_titles = c("Cover", "Contents", "Notes", "Table_1", "Table_2"),
+#'   sheet_types = c("cover", "contents", "notes", "tables", "tables"),
+#'   sheet_titles = c(
+#'     "The 'aftables' Demo Workbook",
+#'     "Table of contents",
+#'     "Notes",
+#'     "Table 1: First Example Sheet",
+#'     "Table 2: Second Example Sheet"
+#'   ),
+#'   blank_cells = c(
+#'     rep(NA_character_, 3),
+#'     "Blank cells indicate that there's no note in that row.",
+#'     NA_character_
+#'   ),
+#'   custom_rows = list(
+#'     NA_character_,
+#'     NA_character_,
+#'     "A custom row.",
+#'     c(
+#'       paste0("First custom row [with a hyperlink.]",
+#'              "(https://best-practice-and-impact.github.io/aftables/)"),
+#'       "Second custom row."
 #'     ),
-#'     blank_cells = c(
-#'       rep(NA_character_, 3),
-#'       "Blank cells indicate that there's no note in that row.",
-#'       NA_character_
-#'     ),
-#'     custom_rows = list(
-#'       NA_character_,
-#'       NA_character_,
-#'       "A custom row.",
-#'       c(
-#'         "First custom row [with a hyperlink.](https://best-practice-and-impact.github.io/aftables/)",
-#'         "Second custom row."
-#'       ),
-#'       "A custom row."
-#'     ),
-#'     sources = c(
-#'       rep(NA_character_, 3),
-#'       "[The Source Material, 2024.](https://best-practice-and-impact.github.io/aftables/)",
-#'       "The Source Material, 2024."
-#'     ),
-#'     tables = list(cover_list, contents_df, notes_df, table_1_df, table_2_df)
-#'   )
+#'     "A custom row."
+#'   ),
+#'   sources = c(
+#'     rep(NA_character_, 3),
+#'     paste0("[The Source Material, 2024.]",#
+#'            "(https://best-practice-and-impact.github.io/aftables/)"),
+#'     "The Source Material, 2024."
+#'   ),
+#'   tables = list(cover_list, contents_df, notes_df, table_1_df, table_2_df)
+#' )
 #'
 #' # Test that 'aftable' is one of the object's classes
 #' is_aftable(x)
