9d6416dd44b7d17b3880fd2f72424c3f9449b7af
Olivia Box Power <Olivia.BoxPower@dhsc.gov.uk>
2026-02-20 09:42:31 +0000

    Add error checks to generate_workbook

--- R/workbook.R
+++ R/workbook.R
@@ -90,6 +90,18 @@ generate_workbook <- function(aftable,
     stop("The object passed to argument 'content' must have class 'aftable'.")
   }
 
+  if(!is.null(author) && !rlang::is_scalar_character(author)) {
+    stop("author must be a character vector of length 1", call. = FALSE)
+  }
+
+  if(!is.null(title) && !rlang::is_scalar_character(title)) {
+    stop("title must be a character vector of length 1", call. = FALSE)
+  }
+
+  if(!is.null(keywords) && !is.character(keywords)) {
+    stop("keywords must be a character vector", call. = FALSE)
+  }
+
   user_config <- list(
     workbook_properties = list(
       author = author,
