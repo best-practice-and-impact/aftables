6f5ed3c138c2336f6a9d73b3915f6b0f2ddef21c
Olivia Box Power <Olivia.BoxPower@dhsc.gov.uk>
2026-07-08 16:47:20 +0100

    Update number_formatter error checks

--- R/utils.R
+++ R/utils.R
@@ -17,21 +17,24 @@
 #' Control how aftables formats data as numbers with decimal places and commas
 #' as thousand separators. This function can be used to overwrite the default
 #' behaviour of aftables, which normally determines the number of decimal places
-#' required from the data in each numeric column, and adds thousand separators
-#' to numeric columns. A key use case for this function is to prevent aftables
-#' from displying calendar or financial years as numbers with thousand
-#' separators, but may be used with any numeric data. Use this function before
-#' the data frame is passed to the aftables::create_aftable function.
+#' required automatically from the data in each numeric column, and adds
+#' thousand separators to numeric columns. A key use case for this function is
+#' to prevent aftables from displying calendar or financial years as numbers
+#' with thousand separators, but it can be used with any numeric data. Use this
+#' function before the data frame is passed to the aftables::create_aftable
+#' function.
 #'
 #' @param table Required data frame. Data frame to be passed into
-#' aftables::create_aftable function. No default.
-#' @param columns Required character vector containing names of columns
-#' or tidyselect pattern determining columns to be processed with specified
-#' number formatting. No default.
+#'   aftables::create_aftable function. No default.
+#' @param columns Required character vector containing names of columns or
+#'   tidyselect pattern determining columns to be processed with specified
+#'   number formatting. No default.
 #' @param decimal_places Required numeric value specifying decimal places to
-#' apply to data in specified columns. No default.
-#' @param thousand_separators Required logical value whether data in
-#' specified columns should be formatted with thousand separators. No default.
+#'   apply to data in specified columns. Default (NULL) is for aftables to
+#'   automatically determine number of decimal places from the data.
+#' @param thousand_separators Required logical value whether data in specified
+#'   columns should be formatted with thousand separators. Default (NULL) is for
+#'   aftables to automatically determine whether to use thosands separators.
 #' @examples
 #' \dontrun{
 #' library(dplyr)
@@ -56,15 +59,15 @@
 #'   )
 #'
 #' # number_formatter columns argument also accepts tidyselect expressions
-#' table_1_df_formatted_tidyselect <- table_1_df
+#' table_1_df_formatted_tidyselect <- table_1_df |>
 #' number_formatter(
 #'   columns = tidyselect::starts_with("Date"),
 #'   thousand_separators = FALSE
 #' )
 #'
-#' # the default precision for "Percentage" column is 7 decimal places
-#' # overruled by number_formatter to display the data in Excel workbook to
-#' # 2 decimal places
+#' # The default precision for "Percentage" column is 7 decimal places
+#' # This is overruled by number_formatter to display the data in Excel workbook
+#' # to 2 decimal places
 #' table_1_df_formatted_2dp <- table_1_df |>
 #'   number_formatter(
 #'     columns = "Percentage",
@@ -90,13 +93,21 @@ number_formatter <- function(table,
                              decimal_places = NULL,
                              thousand_separators = NULL) {
 
-  # error check decimal_places and thousand_separators must be length 1
+  # error check decimal_places and thousand_separators are length 1
   if (length(decimal_places) > 1) {
-    stop("`decimal_places` must be of length 1.")
+    stop("`decimal_places` must be of length 1.", call. = FALSE)
+  }
+
+  if (!is.null(decimal_places) && !is.numeric(decimal_places)) {
+    stop("`decimal_places` must be numeric.", call. = FALSE)
   }
 
   if (length(thousand_separators) > 1) {
-    stop("`thousand_separators` must be of length 1.")
+    stop("`thousand_separators` must be of length 1.", call. = FALSE)
+  }
+
+  if (!is.null(thousand_separators) && !is.logical(thousand_separators)) {
+    stop("`thousand_separators` must be TRUE or FALSE", call. = FALSE)
   }
 
   output <- table |>
